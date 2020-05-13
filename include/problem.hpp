5f1e639f7cc58e2fab74ab2bb3a84c5091ce33f5
Mark Vousden <m.vousden@soton.ac.uk>
2020-05-13 17:01:41 +0100

    Incorporate an empty, default problem definition file.

--- include/problem.hpp
+++ include/problem.hpp
@@ -1,15 +1,23 @@
 #ifndef PROBLEM_HPP
 #define PROBLEM_HPP
 
+#include "problem_definition.hpp"
 #include "nodes.hpp"
+
+/* Complain loudly if we don't know how large the application or hardware sizes
+ * are. This indicates a malformed problem definition file. */
+#if !defined(APPLICATION_SIZE) || !defined(HARDWARE_SIZE)
+#error "Application graph or hardware graph size is not defined. Have you \
+provided a legal problem definition file? This is needed at compile time."
+#endif
 
 #include <array>
 
 class Problem
 {
-    std::array<NodeA, 10> nodeAs;
-    std::array<NodeH, 10> nodeHs;
-    std::array<std::array<NodeH, 10>, 10> edgeCacheH;
+    std::array<NodeA, APPLICATION_SIZE> nodeAs;
+    std::array<NodeH, HARDWARE_SIZE> nodeHs;
+    std::array<std::array<NodeH, HARDWARE_SIZE>, HARDWARE_SIZE> edgeCacheH;
     unsigned pMax = std::numeric_limits<unsigned>::max();
 };
 
