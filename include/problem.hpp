4e258a6742f3496eef9b9b45a58e9d9b0aa1b513
Mark Vousden <m.vousden@soton.ac.uk>
2020-05-13 16:47:44 +0100

    Declare Problem::pMax with default definition.

--- include/problem.hpp
+++ include/problem.hpp
@@ -10,6 +10,7 @@ class Problem
     std::array<NodeA, 10> nodeAs;
     std::array<NodeH, 10> nodeHs;
     std::array<std::array<NodeH, 10>, 10> edgeCacheH;
+    unsigned pMax = std::numeric_limits<unsigned>::max();
 };
 
 #endif
