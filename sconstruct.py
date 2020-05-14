7bd1c0dda8315d35775f2c404e4d1cea3b39c1a5
Mark Vousden <m.vousden@soton.ac.uk>
2020-05-14 15:55:40 +0100

    Remove unused import in scons script.

--- sconstruct.py
+++ sconstruct.py
@@ -1,4 +1,3 @@
-import os
 env = Environment(CXX="clang++", CXXFLAGS="-std=c++17 -O0 -Wall")
 sources = Glob("src/*.cpp", exclude="src/problem_definition.cpp")
 env.Program(target="psap-run", source=sources, CPPPATH="./include/")
