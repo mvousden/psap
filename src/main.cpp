7b47fc47cd2c2c0faad138766c636ed87986f1ae
Mark Vousden <m.vousden@soton.ac.uk>
2020-05-14 17:57:33 +0100

    Add a toy behaviour to main, just to demonstrate that it doesn't work and I've achieved nothing so far.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include "problem_definition_wrapper.hpp"
 
+#include <iostream>
+
 int main(int argc, char** argv)
 {
     Problem problem;
     problem_definition::define(problem);
     problem.populate_edge_cache();
     problem.initial_condition_random();
+    std::cout << "The fitness for the initial solution is "
+              <<  problem.compute_total_fitness()
+              << "."
+              << std::endl;
     return 0;
 }
