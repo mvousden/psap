11b2286a7cb5c06df72c0feca42c2924a3ed8e9a
Mark Vousden <m.vousden@soton.ac.uk>
2020-05-22 17:28:13 +0100

    More flexible loading colours, and duplicate edge handling.

--- postprocessing/postprocess.py
+++ postprocessing/postprocess.py
@@ -30,7 +30,7 @@ def check_expected_files(inputDir):
     return rc
 
 
-def colour_from_loadings(loadings, baseColor="#FF0000"):
+def colour_from_loadings(loadings, maxLoading=None, baseColor="#FF0000"):
     """Computes colors given loading values.
 
     Given an array of loading values (loadings), returns an array of
@@ -39,7 +39,8 @@ def colour_from_loadings(loadings, baseColor="#FF0000"):
     with zero loading uses white (#FFFFFF).
 
     This is achieved through clever sneaky use of the alpha channel."""
-    maxLoading = max(loadings)
+    if maxLoading is None:
+        maxLoading = max(loadings)
     return [baseColor + hex(int(loading / maxLoading * 255))[2:]
             for loading in loadings]
 
@@ -64,11 +65,49 @@ def doit(inputDir):
     nodeHLoad = [len(mapAToH[mapAToH["Hardware node name"] == nodeH])
                  for nodeH in nodeHs]
 
-    nodeColours = colour_from_loadings(nodeHLoad)
+    # Compute maximum loading (assume that the maximum loading exists in the
+    # initial placement, but if that's not true, you can always change it
+    # here).
+    maxNodeHLoad = max(nodeHLoad)
 
-    # Initial application node mapping - defines edge weights. <!>
+    nodeColours = colour_from_loadings(nodeHLoad, maxNodeHLoad)
+
+    # Initial application node mapping - defines application edges.
     graphAH = pd.read_csv(os.path.join(inputDir,
                                        filePaths["initial_a_h_graph"]))
+
+    # Create a datastructure holding all neighbours given an edge, only holding
+    # the edges in one direction (by comparing the name strings with '>'). We
+    # create this data structure to avoid overlaying application-mapped
+    # hardware graph edges (AH edges) on top of edges in the hardware graph (H
+    # edges).
+    #
+    # Firstly, populate the hardware edges
+    edgeFromNode = {}
+    for edge in edgeHs:
+        primary = min(*edge)
+        secondary = max(*edge)
+        for node in edge:  # Import to add both primary and secondary, because
+                           # it's possible that a node only appears as a
+                           # secondary node.
+            if node not in edgeFromNode.keys():
+                edgeFromNode[node] = set()
+        edgeFromNode[primary].add(secondary)
+
+    # For each edge in the AH graph, store it in the edgeAHs array if it is not
+    # a duplicate of either an H edge or an AH edge.
+    edgeAHs = []
+    for index, edge in graphAH.iterrows():  # Sorry Pandas purists
+        nodes = [edge["Hardware node name (first)"],
+                 edge["Hardware node name (second)"]]
+        primary = min(*nodes)
+        secondary = max(*nodes)
+
+        # Only draw AH edges if there is no overlapping H edge, and if there is
+        # no reverse AH edge.
+        if secondary not in edgeFromNode[primary]:
+            edgeAHs.append(nodes + [edge["Loading"]])
+            edgeFromNode[primary].add(secondary)
 
     # Draw pretty picture.
     graph = gv.Graph("G", filename="postprocess.gv", engine="circo")
@@ -84,6 +123,14 @@ def doit(inputDir):
     # Hardware edges
     for edgeIndex in range(len(edgeHs)):
         graph.edge(*edgeHs[edgeIndex])
+
+    # Application-hardware edges
+    edgeAHColour = "#FF0000"
+    for edgeIndex in range(len(edgeAHs)):
+        graph.edge(*edgeAHs[edgeIndex][:2], color=edgeAHColour,
+                   label=str(edgeAHs[edgeIndex][2]), fontcolor=edgeAHColour,
+                   constraint="false")
+
     graph.render()
 
 
