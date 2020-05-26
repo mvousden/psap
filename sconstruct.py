9c0287506d31a0cb42bb093632273b40d29409fb
Mark Vousden <m.vousden@soton.ac.uk>
2020-05-26 20:34:15 +0100

    Add pthread linkflag

--- sconstruct.py
+++ sconstruct.py
@@ -4,7 +4,8 @@ env = Environment()
 env["SYSTEM"] = platform.system().lower()
 
 if env["SYSTEM"] == "linux":  # Clang, GCC
-    env.Append(CXXFLAGS="-std=c++17 -O3 -Wall -Wextra -pedantic")
+    env.Append(CXXFLAGS="-std=c++17 -O3 -Wall -Wextra -pedantic",
+               LINKFLAGS="-pthread")
 if env["SYSTEM"] == "windows":  # MSVC
     env.Append(CXXFLAGS="/std:c++17 /O2 /W4 /EHs /Za")
 
