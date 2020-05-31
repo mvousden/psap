65588f38088f5ba4351d6f8e70a1c2ce55c523f3
Mark Vousden <m.vousden@soton.ac.uk>
2020-05-31 18:27:08 +0100

    Add required includes for problem definition. This is clearly not sustainable, but I'm leaving this for now.

--- include/problem_definition_wrapper.hpp
+++ include/problem_definition_wrapper.hpp
@@ -1,5 +1,9 @@
 #ifndef PROBLEM_DEFINITION_WRAPPER_HPP
 #define PROBLEM_DEFINITION_WRAPPER_HPP
 #include "problem.hpp"
+#include <array>
+#include <iomanip>
+#include <map>
+#include <sstream>
 namespace problem_definition{void define(Problem& problem);}
 #endif
