925918f4e9044e233b1f2fdb23a25b44b97631c3
Mark Vousden <m.vousden@soton.ac.uk>
2020-05-31 18:27:24 +0100

    Clean compile of grid example on MSVC.

--- problem_definition_examples/grid.cpp
+++ problem_definition_examples/grid.cpp
@@ -10,13 +10,12 @@ problem.name = "grid_poets";
  * Be wary of changing numMailboxes - if you do, you will also need to change
  * the definitions used by hIndexGivenPos (later). */
 constexpr decltype(problem.nodeAs)::size_type gridDiameter = 1000;
-constexpr decltype(problem.nodeHs)::size_type totMailboxes, boardOuterRange,
-    boardInnerRange, mboxOuterRange, mboxInnerRange;
-boardOuterRange = 3;
-boardInnerRange = 2;
-mboxOuterRange = 4;
-mboxInnerRange = 4;
-totMailboxes = boardOuterRange * boardInnerRange *
+constexpr decltype(problem.nodeHs)::size_type boardOuterRange = 3;
+constexpr decltype(boardOuterRange) boardInnerRange = 2;
+constexpr decltype(boardOuterRange) mboxOuterRange = 4;
+constexpr decltype(boardOuterRange) mboxInnerRange = 4;
+constexpr decltype(boardOuterRange) totMailboxes =
+    boardOuterRange * boardInnerRange *
     mboxOuterRange * mboxInnerRange;  // 3 * 2 * 4 * 4 = 96
 
 /* Define maximum number of application nodes permitted on a hardware node,
@@ -25,12 +24,14 @@ totMailboxes = boardOuterRange * boardInnerRange *
 problem.pMax = 4 * 16 * 128;  // 4 * 16 * 128 = 8192
 
 /* Vector sizing */
-problem.nodeAs.reserve(gridDiameter ** 2);
+problem.nodeAs.reserve(gridDiameter * gridDiameter);
 problem.nodeHs.reserve(totMailboxes);
 
-/* Define directions for connectivity of application and hardware nodes. */
-const std::array<auto, 4> directions = {"outer+", "outer-",
-                                        "inner+", "inner-"};
+/* Define directions for connectivity of application and hardware nodes. Beware
+ * of code duplication! */
+enum class Dir {outerP, outerN, innerP, innerN};
+const std::array<Dir, 4> directions = {Dir::outerP, Dir::outerN,
+                                       Dir::innerP, Dir::innerN};
 
 /* Create a nested array that identifies the index (in nodeAs) of an
  * application node given its (context-sensitive) position in the grid. */
@@ -38,8 +39,8 @@ std::array<std::array<decltype(problem.nodeAs)::size_type, gridDiameter>,
            gridDiameter> aIndexGivenPos;
 
 /* Create application nodes */
-decltype(gridDiameter) aInnerIndex, aOuterIndex;
-auto locWidth = std::string(gridDiameter).size();
+decltype(problem.nodeAs)::size_type aInnerIndex, aOuterIndex;
+auto locWidth = std::to_string(gridDiameter).size();
 for (aOuterIndex = 0; aOuterIndex < gridDiameter; aOuterIndex++)
 {
     for (aInnerIndex = 0; aInnerIndex < gridDiameter; aInnerIndex++)
@@ -65,30 +66,34 @@ for (aOuterIndex = 0; aOuterIndex < gridDiameter; aOuterIndex++)
         for (const auto& direction : directions)
         {
             /* Bounds checking. */
-            if ((direction == "outer+" and aOuterIndex == gridDiameter - 1) or
-                (direction == "outer-" and aOuterIndex == 0) or
-                (direction == "inner+" and aInnerIndex == gridDiameter - 1) or
-                (direction == "inner-" and aInnerIndex == 0)) continue;
+            if ((direction == Dir::outerP and
+                 aOuterIndex == gridDiameter - 1) or
+                (direction == Dir::outerN and aOuterIndex == 0) or
+                (direction == Dir::innerP and
+                 aInnerIndex == gridDiameter - 1) or
+                (direction == Dir::innerN and aInnerIndex == 0)) continue;
 
             /* Compute/lookup neighbour index. */
-            decltype(gridDiameter) nIndex;
+            decltype(aInnerIndex) nIndex;
             switch (direction)
             {
-                case "outer+": nIndex = aIndexGivenPos.at(aOuterIndex + 1)\
-                                   .at(aInnerIndex);
-                               break;
+                case Dir::outerP: nIndex = aIndexGivenPos.at(aOuterIndex + 1)\
+                                     .at(aInnerIndex);
+                                  break;
 
-                case "outer-": nIndex = aIndexGivenPos.at(aOuterIndex - 1)\
-                                   .at(aInnerIndex);
-                               break;
+                case Dir::outerN: nIndex = aIndexGivenPos.at(aOuterIndex - 1)\
+                                     .at(aInnerIndex);
+                                  break;
 
-                case "inner+": nIndex = aIndexGivenPos.at(aOuterIndex)   \
-                                   .at(aInnerIndex + 1);
-                               break;
+                case Dir::innerP: nIndex = aIndexGivenPos.at(aOuterIndex)\
+                                     .at(aInnerIndex + 1);
+                                  break;
 
-                case "inner-": nIndex = aIndexGivenPos.at(aOuterIndex)\
-                                   .at(aInnerIndex - 1);
-                               break;
+                case Dir::innerN: nIndex = aIndexGivenPos.at(aOuterIndex)\
+                                     .at(aInnerIndex - 1);
+                                  break;
+
+                default: nIndex = std::numeric_limits<decltype(nIndex)>::max();
             }
 
             /* Connect */
@@ -107,8 +112,8 @@ for (aOuterIndex = 0; aOuterIndex < gridDiameter; aOuterIndex++)
  *  3. Mailbox vertical (inner) co-ordinate
  * These "co-ordinates" (for lack of a better word) are the values in array
  * key. */
-std::map<std:array<decltype(problem.nodeHs)::size_type, 4>,  // Co-ordinate
-         decltype(problem.nodeHs)::size_type> hIndexGivenPos // Index
+std::map<std::array<decltype(boardOuterRange), 4>,
+         decltype(problem.nodeHs)::size_type> hIndexGivenPos;
 
 /* Create hardware nodes */
 for (decltype(problem.nodeHs)::size_type boardOuterIdx = 0;
@@ -123,29 +128,29 @@ for (decltype(problem.nodeHs)::size_type mboxInnerIdx = 0;
     /* Get index and determine position (outer = horizontal, inner =
      * vertical) */
     unsigned hIndex = static_cast<unsigned>(problem.nodeHs.size());
-    float posHoriz = boardOuterIdx * mboxOuterRange + mboxOuterIdx;
-    float posVerti = boardInnerIdx * mboxInnerRange + mboxInnerIdx;
+    float posHoriz = static_cast<float>(boardOuterIdx * mboxOuterRange +
+                                        mboxOuterIdx);
+    float posVerti = static_cast<float>(boardInnerIdx * mboxInnerRange +
+                                        mboxInnerIdx);
     std::stringstream name;
     name << "H_" << boardOuterIdx
          << "_" << boardInnerIdx
          << "_" << mboxOuterIdx
          << "_" << mboxInnerIdx;
     problem.nodeHs.push_back(std::make_shared<NodeH>(name.str(), hIndex,
-                                                     posHosiz, posVerti));
+                                                     posHoriz, posVerti));
     hIndexGivenPos[{boardOuterIdx, boardInnerIdx,
                     mboxOuterIdx, mboxInnerIdx}] = hIndex;
 }}}}
 
 /* Connect mailboxes within each board up appropriately. */
 float interMboxWeight = 100;
-for (decltype(problem.nodeHs)::size_type boardOuterIdx = 0;
-     boardOuterIdx < boardOuterRange; boardOuterIdx++){
-for (decltype(problem.nodeHs)::size_type boardInnerIdx = 0;
-     boardInnerIdx < boardInnerRange; boardInnerIdx++){
-for (decltype(problem.nodeHs)::size_type mboxOuterIdx = 0;
-     mboxOuterIdx < mboxOuterRange; mboxOuterIdx++){
-for (decltype(problem.nodeHs)::size_type mboxInnerIdx = 0;
-     mboxInnerIdx < mboxInnerRange; mboxInnerIdx++)
+decltype(problem.nodeHs)::size_type boardOuterIdx, boardInnerIdx;
+decltype(problem.nodeHs)::size_type mboxOuterIdx, mboxInnerIdx;
+for (boardOuterIdx = 0; boardOuterIdx < boardOuterRange; boardOuterIdx++){
+for (boardInnerIdx = 0; boardInnerIdx < boardInnerRange; boardInnerIdx++){
+for (mboxOuterIdx = 0; mboxOuterIdx < mboxOuterRange; mboxOuterIdx++){
+for (mboxInnerIdx = 0; mboxInnerIdx < mboxInnerRange; mboxInnerIdx++)
 {
     /* Lookup index of this mailbox, and get a weak pointer. */
     auto hIndex = hIndexGivenPos.at({boardOuterIdx, boardInnerIdx,
@@ -156,52 +161,54 @@ for (decltype(problem.nodeHs)::size_type mboxInnerIdx = 0;
     for (const auto& direction : directions)
     {
         /* Bounds checking. */
-        if ((direction == "outer+" and mboxOuterIdx == mboxOuterRange - 1) or
-            (direction == "outer-" and mboxOuterIdx == 0) or
-            (direction == "inner+" and mboxInnerIdx == mboxInnerRange - 1) or
-            (direction == "inner-" and mboxInnerIdx == 0)) continue;
+        if ((direction == Dir::outerP and mboxOuterIdx == mboxOuterRange - 1) or
+            (direction == Dir::outerN and mboxOuterIdx == 0) or
+            (direction == Dir::innerP and mboxInnerIdx == mboxInnerRange - 1) or
+            (direction == Dir::innerN and mboxInnerIdx == 0)) continue;
 
         /* Compute/lookup neighbour index. */
         decltype(hIndex) nIndex;
         switch (direction)
         {
-            case "outer+": nIndex = hIndexGivenPos.at(
-                {boardOuterIdx, boardInnerIdx,
-                 mboxOuterIdx + 1, mboxInnerIdx});
-                break;
+            case Dir::outerP: nIndex = hIndexGivenPos.at(
+                                           {boardOuterIdx, boardInnerIdx,
+                                            mboxOuterIdx + 1, mboxInnerIdx});
+                                       break;
 
-            case "outer-": nIndex = hIndexGivenPos.at(
-                {boardOuterIdx, boardInnerIdx,
-                 mboxOuterIdx - 1, mboxInnerIdx});
-                break;
+            case Dir::outerN: nIndex = hIndexGivenPos.at(
+                                           {boardOuterIdx, boardInnerIdx,
+                                            mboxOuterIdx - 1, mboxInnerIdx});
+                                       break;
 
-            case "inner+": nIndex = hIndexGivenPos.at(
-                {boardOuterIdx, boardInnerIdx,
-                 mboxOuterIdx, mboxInnerIdx + 1});
-                break;
+            case Dir::innerP: nIndex = hIndexGivenPos.at(
+                                           {boardOuterIdx, boardInnerIdx,
+                                            mboxOuterIdx, mboxInnerIdx + 1});
+                                       break;
 
-            case "inner-": nIndex = hIndexGivenPos.at(
-                {boardOuterIdx, boardInnerIdx,
-                 mboxOuterIdx, mboxInnerIdx - 1});
-                break;
+            case Dir::innerN: nIndex = hIndexGivenPos.at(
+                                           {boardOuterIdx, boardInnerIdx,
+                                            mboxOuterIdx, mboxInnerIdx - 1});
+                                       break;
+
+            default: nIndex = std::numeric_limits<decltype(nIndex)>::max();
         }
 
         /* Connect */
-        problem.edgeHs.push_back(std::tuple(hIndex, nIndex, interMboxWeight));
+        problem.edgeHs.push_back(std::tuple(
+            static_cast<unsigned>(hIndex),
+            static_cast<unsigned>(nIndex), interMboxWeight));
+    }
 }}}}
 
 /* Connect mailboxes that cross boards appropriately. */
 float interBoardWeight = 800;
-for (decltype(problem.nodeHs)::size_type boardOuterIdx = 0;
-     boardOuterIdx < boardOuterRange; boardOuterIdx++){
-for (decltype(problem.nodeHs)::size_type boardInnerIdx = 0;
-     boardInnerIdx < boardInnerRange; boardInnerIdx++){
+for (boardOuterIdx = 0; boardOuterIdx < boardOuterRange; boardOuterIdx++){
+for (boardInnerIdx = 0; boardInnerIdx < boardInnerRange; boardInnerIdx++){
 
     /* Attempt to make a connection for each direction in the topology. Unlike
      * the previous loops, this one is unrolled because each iteration is
      * considerably different. */
     decltype(boardOuterIdx) neighbourOuterIdx, neighbourInnerIdx;
-    decltype(boardOuterIdx) mboxOuterIdx, mboxInnerIdx;
 
     /* Firstly, consider the outer+ direction. Bounds check: */
     if (boardOuterIdx != boardOuterRange - 1)
@@ -218,8 +225,9 @@ for (decltype(problem.nodeHs)::size_type boardInnerIdx = 0;
                 mboxOuterRange - 1, mboxInnerIdx});
             auto nIndex = hIndexGivenPos.at({neighbourOuterIdx,
                 neighbourInnerIdx, 0, mboxInnerIdx});
-            problem.edgeHs.push_back(std::tuple(hIndex, nIndex,
-                                                interBoardWeight));
+            problem.edgeHs.push_back(std::tuple(
+                static_cast<unsigned>(hIndex),
+                static_cast<unsigned>(nIndex), interBoardWeight));
         }
     }
 
@@ -238,8 +246,9 @@ for (decltype(problem.nodeHs)::size_type boardInnerIdx = 0;
                 0, mboxInnerIdx});
             auto nIndex = hIndexGivenPos.at({neighbourOuterIdx,
                 neighbourInnerIdx, mboxOuterRange - 1, mboxInnerIdx});
-            problem.edgeHs.push_back(std::tuple(hIndex, nIndex,
-                                                interBoardWeight));
+            problem.edgeHs.push_back(std::tuple(
+                static_cast<unsigned>(hIndex),
+                static_cast<unsigned>(nIndex), interBoardWeight));
         }
     }
 
@@ -258,8 +267,9 @@ for (decltype(problem.nodeHs)::size_type boardInnerIdx = 0;
                 mboxOuterIdx, mboxInnerRange - 1});
             auto nIndex = hIndexGivenPos.at({neighbourOuterIdx,
                 neighbourInnerIdx, mboxOuterIdx, 0});
-            problem.edgeHs.push_back(std::tuple(hIndex, nIndex,
-                                                interBoardWeight));
+            problem.edgeHs.push_back(std::tuple(
+                static_cast<unsigned>(hIndex),
+                static_cast<unsigned>(nIndex), interBoardWeight));
         }
     }
 
@@ -277,9 +287,10 @@ for (decltype(problem.nodeHs)::size_type boardInnerIdx = 0;
             auto hIndex = hIndexGivenPos.at({boardOuterIdx, boardInnerIdx,
                 mboxOuterIdx, 0});
             auto nIndex = hIndexGivenPos.at({neighbourOuterIdx,
-                neighbourInnerIdx, mboxOuterIdx, mboxInnerRange - 1});                
-            problem.edgeHs.push_back(std::tuple(hIndex, nIndex,
-                                                interBoardWeight));
+                neighbourInnerIdx, mboxOuterIdx, mboxInnerRange - 1});
+            problem.edgeHs.push_back(std::tuple(
+                static_cast<unsigned>(hIndex),
+                static_cast<unsigned>(nIndex), interBoardWeight));
         }
     }
 }}
