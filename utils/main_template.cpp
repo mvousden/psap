4aa3a24e4f52cda74e12362a284b59f5a440e5cd
Mark Vousden <m.vousden@soton.ac.uk>
2020-08-02 10:33:52 +0100

    Add some convenience utilities for running batches of jobs on our big compute machine.

---
+++ utils/main_template.cpp
@@ -0,0 +1,135 @@
+#include "problem_definition_wrapper.hpp"
+#include "parallel_annealer.hpp"
+#include "serial_annealer.hpp"
+
+#include <filesystem>
+#include <iostream>
+
+int main()
+{
+    /* Mouse mode - useful for runtime measurements. */
+    bool mouseMode = {{MOUSE_MODE}};
+
+    /* Whether or not to anneal in serial, or parallel. */
+    bool serial = {{SERIAL_MODE}};
+
+    /* Construct problem */
+    Problem problem;
+    problem_definition::define(problem);
+
+    /* Directory to write to - clear it. */
+    std::filesystem::path outDir = "";
+    if (!mouseMode)
+    {
+        decltype(outDir) outRootDir = "output";
+        outDir = outRootDir / problem.name;
+        problem.define_output_path(outDir);
+        problem.initialise_logging();
+    }
+
+    /* Prepare problem for annealing */
+    problem.initialise_edge_cache(
+        static_cast<unsigned>(problem.nodeHs.size()));
+    problem.populate_edge_cache();
+    problem.initial_condition_random();
+
+    Iteration maxIteration = static_cast<Iteration>({{ITERATIONS}});
+    if (!mouseMode)
+    {
+        /* Check/write integrity */
+        problem.write_integrity_check_errs(
+            (outDir / "integrity_before.err").u8string());
+
+        /* Compute starting fitness for logging */
+        {
+            std::stringstream message;
+            message << "Initial fitness: "
+                    << problem.compute_total_fitness() << ".";
+            problem.log(message.str());
+        }
+
+        /* Write initial condition data */
+        problem.write_a_degrees((outDir / "a_degrees.csv").u8string());
+        problem.write_a_h_graph(
+            (outDir / "initial_a_h_graph.csv").u8string());
+        problem.write_a_to_h_map(
+            (outDir / "initial_a_to_h_map.csv").u8string());
+
+        /* Begin to solve the problem. */
+        {
+            std::stringstream message;
+            message << "Annealing problem for " << maxIteration
+                    << " iterations.";
+            problem.log(message.str());
+        }
+    }
+
+    /* Create the annealer and do the dirty. */
+    if (!mouseMode)
+    {
+        /* Run noisily with much logging and outputting of files. */
+        if (serial)
+        {
+            SerialAnnealer<ExpDecayDisorder>(maxIteration, outDir)(problem);
+        }
+        else
+        {
+            /* Take intermediate fitness measurements. */
+            ParallelAnnealer<ExpDecayDisorder>
+                ({{NUM_THREADS}}, maxIteration, outDir)
+                (problem, maxIteration / 20);
+        }
+    }
+
+    else
+    {
+        /* Run as quietly as possible, printing timing information only. */
+        if (serial)
+        {
+            auto annealer = SerialAnnealer<ExpDecayDisorder>(maxIteration);
+            auto timeAtStart = std::chrono::steady_clock::now();
+            annealer(problem);
+            std::cout << std::chrono::duration_cast<std::chrono::seconds>(
+                std::chrono::steady_clock::now() - timeAtStart).count()
+                      << std::endl;
+        }
+        else
+        {
+            auto annealer = ParallelAnnealer<ExpDecayDisorder>({{NUM_THREADS}},
+                                                               maxIteration);
+            auto timeAtStart = std::chrono::steady_clock::now();
+            annealer(problem);
+            std::cout << std::chrono::duration_cast<std::chrono::seconds>(
+                std::chrono::steady_clock::now() - timeAtStart).count()
+                      << std::endl;
+        }
+
+    }
+
+    /* Write solved stuff */
+    if (!mouseMode)
+    {
+        problem.log("Annealing complete.");
+
+        /* Write solved stuff */
+        {
+            std::stringstream message;
+            message << "Final fitness: "
+                    << problem.compute_total_fitness() << ".";
+            problem.log(message.str());
+        }
+
+        problem.write_a_h_graph((outDir / "final_a_h_graph.csv").u8string());
+        problem.write_a_to_h_map((outDir / "final_a_to_h_map.csv").u8string());
+        problem.write_h_graph((outDir / "h_graph.csv").u8string());
+        problem.write_h_nodes((outDir / "h_nodes.csv").u8string());
+        problem.write_integrity_check_errs(
+            (outDir / "integrity_after.err").u8string());
+
+        /* This one isn't needed, but makes creating histograms easier. */
+        problem.write_h_node_loading(
+            (outDir / "h_node_loading.csv").u8string());
+    }
+
+    return 0;
+}
---
+++ utils/run-many.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# This script runs a series of placement jobs. It, along with
+# main_template.cpp, need to be deployed to the root directory of the
+# repository. This script must also be called from that directory (I'm too lazy
+# to write the chdir logic).
+#
+# Read and understand before running.
+set -e
+set -x
+
+TEMPLATE_SOURCE="main_template.cpp"
+TEMPLATE_TARGET="src/main.cpp"
+ITERATIONS=5e8
+
+# Mouse mode disables all logging and intermediate fitness computation, and
+# simply prints out the runtime of the placement job to a text file.
+MOUSE_MODE=1
+MOUSE_FILE="mouse_out.txt"
+
+if [ ${MOUSE_MODE} -eq 1 ]; then
+    > "${MOUSE_FILE}"
+fi
+
+# Run one serial placement job (THREAD_COUNT = 0), and one parallel placement
+# job for different numbers of compute workers.
+for THREAD_COUNT in 0 1 $(seq 4 4 64); do
+
+    # Deploy template, and provision.
+    cp "${TEMPLATE_SOURCE}" "${TEMPLATE_TARGET}"
+    sed -i "s|{{ITERATIONS}}|$ITERATIONS|" "${TEMPLATE_TARGET}"
+
+    if [ ${MOUSE_MODE} -eq 1 ]; then
+	    sed -i "s|{{MOUSE_MODE}}|true|" "${TEMPLATE_TARGET}"
+    else
+	    sed -i "s|{{MOUSE_MODE}}|false|" "${TEMPLATE_TARGET}"
+    fi
+
+    if [ ${THREAD_COUNT} -eq 0 ]; then
+        sed -i "s|{{SERIAL_MODE}}|true|" "${TEMPLATE_TARGET}"
+        sed -i "s|{{NUM_THREADS}}|1|" "${TEMPLATE_TARGET}"
+    else
+        sed -i "s|{{SERIAL_MODE}}|false|" "${TEMPLATE_TARGET}"
+        sed -i "s|{{NUM_THREADS}}|$THREAD_COUNT|" "${TEMPLATE_TARGET}"
+    fi
+
+    # Build
+	scons -j 4
+
+    # Run
+    if [ ${MOUSE_MODE} -eq 1 ]; then
+        # Pushing output to file.
+	    printf "${THREAD_COUNT}," >> ${MOUSE_FILE}
+	    ./psap-run >> ${MOUSE_FILE}
+    else
+        # Log angrily, moving results afterwards.
+	    ./psap-run
+	    mv output/grid_poets output/grid_poets_${THREAD_COUNT}_$(date --iso-8601=date)_$(git rev-parse HEAD)
+    fi
+done
