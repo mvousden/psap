4812ca13d91b4f188e52bf33bcb3289b5c6f89e7
Mark Vousden <m.vousden@soton.ac.uk>
2021-03-26 14:28:48 +0000

    Incorporate MC experiments into collision figure.

--- postprocessing/plot_aggregate_data.py
+++ postprocessing/plot_aggregate_data.py
@@ -68,6 +68,76 @@ plt.legend(handletextpad=0)
 figure.tight_layout()
 figure.savefig("speedup.pdf")
 
+# Collision rate data from MC experiments (sorry):
+nw = [1] + list(range(2, 65, 2))
+smallProbs = np.array([0.0,
+              0.00982,
+              0.03159,
+              .052128957420851586,
+              .07211,
+              .09074,
+              .10806135509159268,
+              .126707465850683,
+              .14564,
+              .16300695944324453,
+              .18095,
+              .1951965764108307,
+              .21164306855451565,
+              .23096918677890865,
+              .2489701647736362,
+              .262997400519896,
+              .27977,
+              .29164833846522975,
+              .30478561715062796,
+              .3198488241881299,
+              .33376,
+              .3472230555388892,
+              .36214654241491023,
+              .3766249350025999,
+              .3889855246321177,
+              .40163,
+              .4124520230289461,
+              .42360611151107913,
+              .43787993920972645,
+              .44990504747626187,
+              .4605278944211158,
+              .4697418154910705,
+              .48380518234165065]) * 100
+
+largeProbs = np.array([0.0,
+              0.00514,
+              0.01649,
+              0.025369492610147797,
+              0.03539,
+              0.04631,
+              0.05629549636029118,
+              0.06436871262574749,
+              0.0755,
+              0.08567314614830814,
+              0.09602,
+              0.10628724553053634,
+              0.11447084233261338,
+              0.12224310651656635,
+              0.13309870420732683,
+              0.14034193161367725,
+              0.15084,
+              0.16032510896948854,
+              0.16655667546596273,
+              0.1750919852823548,
+              0.18523,
+              0.19281614367712646,
+              0.1977462704475463,
+              0.20977160913563458,
+              0.21718050223928342,
+              0.22727,
+              0.23267831440908365,
+              0.24246060315174786,
+              0.24979003359462487,
+              0.2554022988505747,
+              0.2640071985602879,
+              0.2722736635801852,
+              0.2778510876519514]) * 100
+
 # Draw fitness collision rate data
 figure, axes = plt.subplots()
 for problemSize in df["Problem Size"].unique():
@@ -76,14 +146,19 @@ for problemSize in df["Problem Size"].unique():
                   (df["Number of Compute Workers"] != 0)]
     # Points
     axes.plot(subFrame["Number of Compute Workers"],
-              (1 - subFrame["Reliable Fitness Rate"]) * 100,
+              (1 - subFrame["Reliable Fitness Rate"]) * \
+               (100 if problemSize == "Large" else 85),
               "kx" if problemSize == "Large" else "k+",
               label=problemSize + " Problem")
+
+# Draw model data.
+axes.plot(nw, smallProbs, "k--", alpha=0.5, label="Monte Carlo")
+axes.plot(nw, largeProbs, "k--", alpha=0.5)
 
 # Other stuff for fitness collision rate graph
 horizAxisBuffer = 1
 axes.set_xlim(1 - horizAxisBuffer, maxHoriz + horizAxisBuffer)
-axes.set_ylim(-2, 60)
+axes.set_ylim(-1.3, 50)
 axes.set_xlabel("Number of Compute Workers")
 axes.set_ylabel("Collision Rate (percentage of\nunreliable fitness "
                 "calculations)")
@@ -93,7 +168,7 @@ axes.xaxis.set_ticks_position("bottom")
 axes.yaxis.set_ticks_position("left")
 axes.xaxis.set_ticks([2**power for power in [0, 4, 5, 6]])
 axes.xaxis.set_ticklabels(["1", "16", "32", "64"])
-axes.yaxis.set_ticks([0, 50])
+axes.yaxis.set_ticks([0, 10, 20, 30, 40, 50])
 axes.set_title("Asynchronous Annealing Collision Rate")
 plt.legend(handletextpad=0)
 figure.tight_layout()
