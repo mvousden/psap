63ab2072925a7ade374812c785f028a6792252a8
Mark Vousden <m.vousden@soton.ac.uk>
2021-04-15 22:44:42 +0100

    Add large-synchronous timing info

--- postprocessing/plot_aggregate_data.py
+++ postprocessing/plot_aggregate_data.py
@@ -18,8 +18,8 @@ import pandas as pd
 import sys
 
 matplotlib.rc("axes", linewidth=1)
-matplotlib.rc("figure", figsize=(4.0, 3.3))
-matplotlib.rc("font", family="serif", size=10)
+matplotlib.rc("figure", figsize=(2.7, 2.7))
+matplotlib.rc("font", family="serif", size=7)
 matplotlib.rc("legend", frameon=False)
 matplotlib.rc("xtick.major", width=1)
 matplotlib.rc("ytick.major", width=1)
@@ -31,42 +31,47 @@ else:
     fPath = sys.argv[1]
 df = pd.read_csv(fPath).sort_values(by="Number of Compute Workers")
 
-# Draw speedup data for the small problem
-figure, axes = plt.subplots()
-for synchronisation in df["Synchronisation"].unique():
-    subFrame = df[(df["Synchronisation"] == synchronisation) &
-                  (df["Problem Size"] == "Small")]
-    serialTime = subFrame[subFrame["Number of Compute Workers"] == 0]\
-        ["Runtime"].values[0]
+# Draw speedup data for the small and large problems separately
+for problem in ["Small", "Large"]:
+    figure, axes = plt.subplots()
+    for synchronisation in df["Synchronisation"].unique():
+        subFrame = df[(df["Synchronisation"] == synchronisation) &
+                      (df["Problem Size"] == problem)]
+        serialTime = subFrame[subFrame["Number of Compute Workers"] == 0]\
+            ["Runtime"].values[0]
 
-    # Points (the 1: slice removes n=0)
-    axes.plot(subFrame["Number of Compute Workers"][1:],
-              serialTime / subFrame["Runtime"][1:],
-              "k." if synchronisation == "Asynchronous" else "k1",
-              label=synchronisation + " Annealer")
+        # Points (the 1: slice removes n=0)
+        axes.plot(subFrame["Number of Compute Workers"][1:],
+                  serialTime / subFrame["Runtime"][1:],
+                  "k." if synchronisation == "Asynchronous" else "k1",
+                  label=synchronisation + " Annealer")
 
-# Draw linear speedup line
-maxHoriz = df["Number of Compute Workers"].max()
-# axes.plot([1, maxHoriz], [1, maxHoriz], alpha=0.2, color="k", linestyle="--")
+    # Draw linear speedup line
+    maxHoriz = df["Number of Compute Workers"].max()
+    # axes.plot([1, maxHoriz], [1, maxHoriz], alpha=0.2, color="k",
+    #           linestyle="--")
 
-# Other stuff for speedup graph
-axisBuffer = 1
-axes.set_xlim(1 - axisBuffer, maxHoriz + axisBuffer)
-axes.set_ylim(0, 10 + axisBuffer)
-axes.set_xlabel("Number of Compute Workers ($n$)")
-axes.set_ylabel("Serial-Relative Speedup ($t_0/t_n$)")
-axes.spines["top"].set_visible(False)
-axes.spines["right"].set_visible(False)
-axes.xaxis.set_ticks_position("bottom")
-axes.yaxis.set_ticks_position("left")
-labels = [2**power for power in [0, 4, 5, 6]]
-axes.xaxis.set_ticks(labels)
-axes.xaxis.set_ticklabels(["1", "16", "32", "64"])
-axes.yaxis.set_ticks([0, 2, 4, 6, 8, 10])
-axes.set_title("Runtime Scaling (Small Problem)")
-plt.legend(handletextpad=0)
-figure.tight_layout()
-figure.savefig("speedup.pdf")
+    # Other stuff for speedup graph
+    axisBuffer = 1
+    axes.set_xlim(1 - axisBuffer, maxHoriz + axisBuffer)
+    axes.set_ylim(0, 10)
+    axes.set_xlabel("Number of Compute Workers")
+    if problem == "Large":
+        axes.set_ylabel("Serial-Relative Speedup ($t_0/t_n$)")
+    axes.spines["top"].set_visible(False)
+    axes.spines["right"].set_visible(False)
+    axes.xaxis.set_ticks_position("bottom")
+    axes.yaxis.set_ticks_position("left")
+    labels = [2**power for power in [0, 4, 5, 6]]
+    axes.xaxis.set_ticks(labels)
+    axes.xaxis.set_ticklabels(["1", "16", "32", "64"])
+    axes.yaxis.set_ticks([0, 1, 2, 4, 6, 8, 10])
+    axes.set_title("Runtime Scaling ({} Problem)".format(problem))
+    if problem == "Large":
+        plt.legend(handletextpad=0)
+    figure.tight_layout()
+    figure.savefig("speedup_{}.pdf".format(problem.lower()))
+    plt.close()
 
 # Collision rate data from MC experiments (sorry):
 nw = [1] + list(range(2, 65, 2))
